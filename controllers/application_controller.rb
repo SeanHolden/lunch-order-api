4864d05cd8176900fdd63ad279ec6f3990860e35
Sean Holden <sean.holden@onthebeach.co.uk>
2015-12-13 19:48:51 +0000

    Did not need to set content type after all

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
 
-  before do
-    content_type 'application/json'
-  end
-
   set :method do |method|
     method = method.to_s.upcase
     condition { request.request_method == method }
