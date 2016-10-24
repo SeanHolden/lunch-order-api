30a3531c164fd8e8fa52b82e828658591eb1e931
Carl Johnstone <carl.johnstone@onthebeach.co.uk>
2016-10-24 17:29:34 +0100

    Put names after orders

--- spec/models/sms/body_spec.rb
+++ spec/models/sms/body_spec.rb
@@ -4,22 +4,20 @@ describe Sms::Body do
   describe '#to_s' do
     subject(:body) { Sms::Body.new.to_s }
 
-    let(:order) { double(Order, text_order: 'Chicken Burger') }
-    let(:todays_orders) { double('TodaysOrders') }
+    let(:order) { double(Order, text_order: 'Chicken Burger', name: 'Dave') }
+    let(:order2) { double(Order, text_order: 'Cheese Burger', name: 'Paul') }
+    let(:order3) { double(Order, text_order: 'Fish Burger', name: 'John') }
 
     before do
       allow(Order).to receive(:todays_orders).and_return(todays_orders)
       allow(todays_orders).to receive(:last).and_return(order)
-      allow(todays_orders).to receive(:pluck).
-        with(:text_order).
-        and_return(text_orders)
     end
 
     context 'multiple orders' do
-      let(:text_orders) {
-        [order.text_order, order.text_order, order.text_order]
+      let(:todays_orders) {
+        [order, order2, order3]
       }
-      let(:expected_body) { "Hello. Could I please place an order to collect around 12.\n1) Chicken Burger\n2) Chicken Burger\n3) Chicken Burger\nThank you!" }
+      let(:expected_body) { "Hello. Could I please place an order to collect around 12.\n1) Chicken Burger (Dave)\n2) Cheese Burger (Paul)\n3) Fish Burger (John)\nThank you!" }
 
       it 'returns text containing numbered list' do
         expect(body.to_s).to eql(expected_body)
@@ -27,10 +25,10 @@ describe Sms::Body do
     end
 
     context 'one single order' do
-      let(:text_orders) {
-        [order.text_order]
+      let(:todays_orders) {
+        [order]
       }
-      let(:expected_body) { "Hello. Could I please place an order to collect around 12.\n- Chicken Burger\nThank you!" }
+      let(:expected_body) { "Hello. Could I please place an order to collect around 12.\n- Chicken Burger (Dave)\nThank you!" }
 
       it 'returns text containing single order preceded by a dash' do
         expect(body.to_s).to eql(expected_body)
